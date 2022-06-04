811066d94208f7ae3c826a9ca09fbcb6d9e48e23
Jesús Ariel Gimenez <jesusariel.gimenez@strata-analytics.us>
2022-06-03 21:21:19 -0300

    add glue resources to the stack

--- cdk/datapipeline_modeltraining_stack.py
+++ cdk/datapipeline_modeltraining_stack.py
@@ -5,11 +5,20 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+from .glue_resources import GlueResources
+
 class DatapipelineModeltrainingStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, config_enviroment:dict, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        self.configurations = config_enviroment
 
+    def deploy_glue_resource(self):
+        glue = GlueResources(self, "GlueResources", self.configurations)
+        glue.deploy()
+
+    def deploy(self):
+        self.deploy_glue_resource()
         # The code that defines your stack goes here
 
         # example resource
