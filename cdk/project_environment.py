61f0c3e1edb00870125d19091b9080b79c9b9b86
Jesús Ariel Gimenez <jesusariel.gimenez@strata-analytics.us>
2022-06-03 21:22:12 -0300

    cleaning

--- cdk/project_environment.py
+++ cdk/project_environment.py
@@ -7,16 +7,17 @@ class ProjectEnvironment():
 
     def __init__(self, env='default') -> None:
         self.env = env        
-        self.loadConfig()
+        self._configurations = self.loadConfig()
         pass
 
     def loadConfig(self):
         env = self.env
         with open(f'config/{env}.yml', 'r') as stream:
             try:
-                self._configurations = yaml.safe_load(stream)
+                configurations = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 raise yaml.YAMLError(exc)
+        return configurations
         
 
 if __name__ == '__main__':
